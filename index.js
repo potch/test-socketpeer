097c6340dfabd6a03f2c03d119e1bbf389903fc2
Matt Claypotch <thepotch@gmail.com>
2015-03-31 10:36:08 -0700

    update for lib changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
-var http = require('http');
 var path = require('path');
 var SocketPeer = require('socketpeer');
 var url = require('url');
 
-var httpServer = http.createServer(function (req, res) {
+var server = new SocketPeer();
+
+server.on('request', function (req, res) {
   var stream;
   var p = url.parse(req.url).pathname;
   if (p === '/') {
@@ -12,19 +13,4 @@ var httpServer = http.createServer(function (req, res) {
     stream = fs.createReadStream(path.join(__dirname, 'index.html'));
     stream.pipe(res);
   }
-  if (p === '/socketpeer.js') {
-    res.writeHead(200, {'Content-Type': 'text/javascript'});
-    stream = fs.createReadStream(path.join(__dirname, 'node_modules/socketpeer/socketpeer.js'));
-    stream.pipe(res);
-  }
-});
-
-var sp = new SocketPeer({
-  httpServer: httpServer
-});
-
-var port = (process.env.PORT || 3000);
-
-httpServer.listen(port, '0.0.0.0', function () {
-  console.log('Server listening on 0.0.0.0:%d', port);
 });
